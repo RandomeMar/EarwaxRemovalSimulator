32153c0570a7d162673a4b6fd375c0144b892a0d
Andrew R <arojas11@unomaha.edu>
2026-04-26 21:36:11 -0500

    Updated Unity Testing

--- AudiologyUnityProject/Assets/_Project/UnityTests/NewTestScript.cs
+++ AudiologyUnityProject/Assets/_Project/UnityTests/NewTestScript.cs
@@ -9,35 +9,65 @@ public class NewTestScript
 {
 
     [UnityTest]
-    public IEnumerator CheckHeadTypeSetting()
+    public IEnumerator CheckEarTypeSetting()
     {
 
-        string head = "Right";
-        PlayerPrefs.SetString("headType", "Right");
+        string ear = "3";
+        PlayerPrefs.SetString("earType", "3");
 
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
-        Assert.AreEqual(PlayerPrefs.GetString("headType"), head);
+        Assert.AreEqual(PlayerPrefs.GetString("earType"), ear);
 
 
         yield return null;
     }
 
     [UnityTest]
-    public IEnumerator CheckCerumenAmountSetting()
+    public IEnumerator CheckBlockTypeSetting()
     {
 
-        int cerumen = 15;
-
-        PlayerPrefs.SetInt("cerumenAmount", 15);
+        string block = "2";
+        PlayerPrefs.SetString("blockType", "2");
 
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
-        Assert.AreEqual(PlayerPrefs.GetInt("cerumenAmount"), cerumen);
+        Assert.AreEqual(PlayerPrefs.GetString("blockType"), block);
 
 
         yield return null;
     }
+
+    [UnityTest]
+    public IEnumerator CheckWaxTypeSetting()
+    {
+
+        string wax = "1";
+        PlayerPrefs.SetString("waxType", "1");
+
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        Assert.AreEqual(PlayerPrefs.GetString("waxType"), wax);
+
+
+        yield return null;
+    }
+
+    // [UnityTest]
+    // public IEnumerator CheckCerumenAmountSetting()
+    // {
+
+    //     int cerumen = 15;
+
+    //     PlayerPrefs.SetInt("cerumenAmount", 15);
+
+    //     // Use the Assert class to test conditions.
+    //     // Use yield to skip a frame.
+    //     Assert.AreEqual(PlayerPrefs.GetInt("cerumenAmount"), cerumen);
+
+
+    //     yield return null;
+    // }
 
     //[UnityTest]
     //public IEnumerator CheckSceneLoading()
